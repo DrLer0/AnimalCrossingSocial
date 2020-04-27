7bbe38a78d76f8ab6d4aef9ab44080403c977bf8
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-04-27 01:22:06 -0700

    pushing initial route setup with Material layout

---
+++ client/src/layouts/Main/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+import { Typography, Link } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const Footer = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <Typography variant="body1">
+        &copy;{' '}
+        <Link
+          component="a"
+          href="/"
+          target="_blank"
+        >
+          Animal Crossing Social
+        </Link>
+        . 2020
+      </Typography>
+      <Typography variant="caption">
+        Disclaimer: All content on this site claims no affiliation with Nintendo Co.
+      </Typography>
+    </div>
+  );
+};
+
+Footer.propTypes = {
+  className: PropTypes.string
+};
+
+export default Footer;
