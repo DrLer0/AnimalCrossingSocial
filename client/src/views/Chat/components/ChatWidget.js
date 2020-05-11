bc449a763d9755467cf27d6f8afa7973996baa69
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-05-11 13:52:36 -0700

    add redux to chatwidget

--- client/src/views/Chat/components/ChatWidget.js
+++ client/src/views/Chat/components/ChatWidget.js
@@ -1,66 +1,51 @@
 import React from 'react';
+import { Link, unstable_StrictModeFade } from '@material-ui/core';
 import { Chat, Channel, ChannelList, Window } from 'stream-chat-react';
 import { ChannelHeader, MessageList } from 'stream-chat-react';
 import { MessageInput, Thread } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
+import { makeStyles } from '@material-ui/styles';
+
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import clsx from 'clsx';
+
 
 import 'stream-chat-react/dist/css/index.css';
 
-// ----------------------------------------------------------
-// move to server.js later when we start registering users/logging in
-const chatClient = new StreamChat('uzy7f9qpss8w', 'g3v4w9qwmxr9epxfv28dakb3ywck8jx3jj7s4evh5hxzfjyuwzm2gqztvnjnxpzj');
-
-// for production use userToken
-// devToken for development
-// const userToken = chatClient.createToken('john');
-chatClient.setUser(
-  {
-      id: 'Jon',
-      name: 'Jon D',
-      image: 'https://getstream.io/random_svg/?name=John',
+const useStyles = makeStyles(theme => ({
+  root: {
+    alignItems: 'center',
+    minHeight: 'fit-content'
   },
-  // userToken
-  chatClient.devToken('Jon')
-);
-// or pass in chatClient from server.js using modules/export
-// ----------------------------------------------------------
-
-// Uncomment to sign in as a different user and comment out the one above
-// ----------------------------------------------------------
-// move to server.js later when we start registering users/logging in
-
-// const chatClient = new StreamChat('r8s5seqdx9c4', '2j47qttc3uxpatpfmcy857zrrgd43rjjw4sawabancvjms6jpgemk5tts82hk38r');
-
-// // for production use userToken
-// // devToken for development
-// // const userToken = chatClient.createToken('Some1');
-// chatClient.setUser(
-//   {
-//       id: 'Some1',
-//       name: 'Any One',
-//       image: 'https://getstream.io/random_svg/?name=John',
-//   },
-//   // userToken
-//   chatClient.devToken('Some1')
-// );
-// // or pass in chatClient from server.js using modules/export
-// // ----------------------------------------------------------
+  avatar: {
+    width: 60,
+    height: 60
+  },
+  name: {
+    marginTop: theme.spacing(1)
+  }
+}));
 
 // custom channel preview component
 class MyChannelPreview extends React.Component {
   render() {
       const {setActiveChannel, channel} = this.props;
-      const unreadCount = channel.countUnread();
+      let unreadCount = channel.countUnread();
+      let unreadDiv;
+
+      if(unreadCount > 0){
+        unreadDiv = <span className="red-text"> - Unread: {unreadCount}</span>
+      } else {
+        unreadDiv = <span className="red-text"></span>
+      }
 
       return (
       <div className="channel_preview">
         <a href="#" onClick={(e) => setActiveChannel(channel, e)}>
           {channel.data.name}
         </a>
-
-        <span>
-          Unread messages: {unreadCount}
-        </span>
+        {unreadDiv}
       </div>
     );
   }
@@ -73,14 +58,39 @@ class MyMessageComponent extends React.Component {
   }
 }
 
-const filters = { type: 'messaging', members: { $in: ['winter-block-0'] } };
-const sort = { last_message_at: -1 };
-const channels = chatClient.queryChannels(filters, sort);
+const ChatApp = (props) => {
+  const { className, ...rest } = props;
+  console.log("+++++++++++++");
+  console.log(props);
+  console.log("+++++++++++++");
 
-const ChatApp = () => (
+  const classes = useStyles();
+
+  const { user } = props.auth;
+  
+  const chatClient = new StreamChat('uzy7f9qpss8w', 'g3v4w9qwmxr9epxfv28dakb3ywck8jx3jj7s4evh5hxzfjyuwzm2gqztvnjnxpzj');
+  chatClient.setUser(
+    {
+        id: user.id,
+        name: user.name.split(" ")[0],
+        image: 'https://getstream.io/random_svg/?name='+user.name.split(" ")[0],
+    },
+    // userToken
+    chatClient.devToken(user.id)
+  );
+  const filters = { type: 'messaging', members: { $in: [user.id] } };
+  const sort = { last_message_at: -1 };
+  const channels = chatClient.queryChannels(filters, sort);
+  // members: { $in: ['winter-block-0'] }
+  return (
+    <div
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
   <Chat client={chatClient} theme={'messaging light'}>
     <ChannelList
-      filters={filters}
+      // filters={filters}
+      channels={channels}
       sort={sort}
       Preview={MyChannelPreview}
     />
@@ -93,6 +103,20 @@ const ChatApp = () => (
       <Thread />
     </Channel>
   </Chat>
-);
+  </div>
+  )
+};
 
-export default ChatApp;
+ChatApp.propTypes = {
+  className: PropTypes.string,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  test: "testChat"
+});
+
+export default connect(
+  mapStateToProps
+)(ChatApp);
