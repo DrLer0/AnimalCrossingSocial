38d0e3d8ef21b0348c3deb2b8d796661d3a50554
Kate Foust <kate.e.foust@gmail.com>
2020-05-12 14:09:35 -0700

    switched mongoURI back to cloud

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongoURI:
-  // "mongodb+srv://animalcrossing123:animalcrossing123@animalcrossingsocial-qz5zv.mongodb.net/test?retryWrites=true&w=majority",
-  mongoURI: "mongodb://localhost/animal-crossing-social",
+  mongoURI:
+    "mongodb+srv://animalcrossing123:animalcrossing123@animalcrossingsocial-qz5zv.mongodb.net/test?retryWrites=true&w=majority",
+  // mongoURI: "mongodb://localhost/animal-crossing-social",
   secretOrKey: "secret",
 };
